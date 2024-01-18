a04665fe40b15bafb89d3289d224112a81d4af5f
Bas Milius <basmilius@hotmail.com>
2024-01-18 21:10:53 +0100

    chore: add class constants.

--- src/Raxos/Terminal/TerminalException.php
+++ src/Raxos/Terminal/TerminalException.php
@@ -15,9 +15,9 @@ use Raxos\Foundation\Error\RaxosException;
 final class TerminalException extends RaxosException
 {
 
-    public const ERR_COMMAND_NOT_FOUND = 1;
-    public const ERR_COMMAND_FAILED = 2;
-    public const ERR_COMMAND_SYNTAX = 4;
-    public const ERR_ILLEGAL = 8;
+    public const int ERR_COMMAND_NOT_FOUND = 1;
+    public const int ERR_COMMAND_FAILED = 2;
+    public const int ERR_COMMAND_SYNTAX = 4;
+    public const int ERR_ILLEGAL = 8;
 
 }
